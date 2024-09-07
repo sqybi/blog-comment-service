99b4e197b664071a838de1f2100df26953028dec
sqybi <sqybi@126.com>
2024-09-08 00:21:49 +0800

    [draft] Send mail

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,8 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
+import { EmailMessage } from 'cloudflare:email';
+import { createMimeMessage } from 'mimetext/browser';
 
 type PostCommentEvent = {
   // The unique identifier of the article, e.g., the last part of the URL, required
@@ -92,6 +94,182 @@ const corsHeaders = {
   'Access-Control-Expose-Headers': 'Content-Length, Content-Range',
 };
 
+// TODO(sqybi): Cloudflare R2 needs a `Origin` header to allow CORS, so <link rel="stylesheet"> and fonts preload will not work.
+const htmlCommon = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <style>
+      body {
+        font-family: "Noto Serif SC", system-ui, -apple-system, Segoe UI, Roboto,
+          Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont,
+          "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji",
+          "Segoe UI Emoji", "Segoe UI Symbol";
+        line-height: 1.6;
+        color: #6b8541;
+      }
+      .container {
+        width: 80%;
+        margin: auto;
+        padding: 20px;
+        width: 600px;
+        border: 1px solid #ddd;
+        border-radius: 10px;
+        background-color: #e4e2d3;
+      }
+      .header {
+        text-align: center;
+        padding-bottom: 20px;
+        color: #ffffff;
+        background-color: #8bac55;
+        padding: 10px;
+        border-radius: 0.3rem;
+        box-shadow: 0 0 5px #6b8541;
+      }
+      .comment,
+      .reply {
+        border: none;
+        border-radius: 4px 20px 4px 4px;
+        box-shadow: 0 0 8px #6b8541;
+        padding: 10px;
+        background-color: #f9f9f9;
+      }
+      .info {
+        border: 1px dashed #6b8541;
+        border-radius: 2px 2px 2px 2px;
+        box-shadow: 0 0 2px #6b8541;
+        font-size: 80%;
+        padding: 10px;
+        background-color: #f9f9f9;
+        text-align: center;
+      }
+      .footer {
+        text-align: center;
+        padding-top: 20px;
+        font-size: 0.9em;
+        color: #777;
+      }
+      .spacing {
+        padding: 10px;
+        background-color: transparent;
+      }
+      .comment-time {
+        font-size: 80%;
+        font-weight: normal;
+        color: #888888;
+        padding: 2px 3px;
+        border: 1px dotted #888888;
+        border-radius: 5px;
+      }
+      .comment h3,
+      .reply h3 {
+        color: #6b8541;
+      }
+      a {
+        color: #8bac55;
+        text-decoration: none;
+        font-weight: bold;
+      }
+      a:hover {
+        color: #6b8541;
+      }
+    </style>
+  </head>
+`;
+
+const htmlCommentReply = `
+  <body>
+    <div class="container">
+      <div class="header">
+        <h2>Your Comment Received a Reply!</h2>
+      </div>
+      <div class="spacing"></div>
+      <div class="comment">
+        <h3>
+          Your Comment
+          <span class="comment-time">@ $ORIGINAL_COMMENT_TIME</span>
+        </h3>
+        <p>$ORIGINAL_COMMENT</p>
+      </div>
+      <div class="spacing"></div>
+      <div class="reply">
+        <h3>
+          Reply from $REPLIED_AUTHOR_NAME
+          <span class="comment-time">@ $REPLIED_COMMENT_TIME</span>
+        </h3>
+        <p>$REPLIED_COMMENT</p>
+      </div>
+      <div class="spacing"></div>
+      <div class="info">Any subsequent replies to your comment will trigger notification emails.</div>
+      <div class="footer">
+        <p>
+          Copyright © 2023-2024 <a href="https://sqybi.com/">SQYBI.com</a><br />
+          Built with Docusaurus. Served by Cloudflare.
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
+`;
+
+const htmlCommentPosted = `
+  <body>
+    <div class="container">
+      <div class="header">
+        <h2>You Posted a New Comment!</h2>
+      </div>
+      <div class="spacing"></div>
+      <div class="comment">
+        <h3>
+          Your Comment
+          <span class="comment-time">@ $ORIGINAL_COMMENT_TIME</span>
+        </h3>
+        <p>$ORIGINAL_COMMENT</p>
+      </div>
+      <div class="spacing"></div>
+      <div class="info">
+        Any subsequent replies to your comment will trigger notification emails.
+      </div>
+      <div class="footer">
+        <p>
+          Copyright © 2023-2024 <a href="https://sqybi.com/">SQYBI.com</a><br />
+          Built with Docusaurus. Served by Cloudflare.
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
+`;
+
+const textCommentReply = `
+Your Comment Received a Reply!
+
+Your Comment @ $ORIGINAL_COMMENT_TIME:
+
+$ORIGINAL_COMMENT
+
+Reply from $REPLIED_AUTHOR_NAME @ $REPLIED_COMMENT_TIME:
+
+$REPLIED_COMMENT
+
+Any subsequent replies to your comment will trigger notification emails.
+
+<Copyright © 2023-2024 SQYBI.com>
+`;
+
+const textCommentPosted = `
+You Posted a New Comment!
+
+Your Comment @ $ORIGINAL_COMMENT_TIME:
+
+$ORIGINAL_COMMENT
+
+Any subsequent replies to your comment will trigger notification emails.
+
+<Copyright © 2023-2024 SQYBI.com>
+`;
+
 const setCorsHeaderToResponse = async (
   request: Request,
   response: Response,
@@ -101,7 +279,27 @@ const setCorsHeaderToResponse = async (
   if (!origin) {
     return new Response('Origin header not found', { status: 400 });
   }
-  if (allowedOrigins.includes(origin)) {
+  let is_allowed = false;
+  for (const allowedOrigin of allowedOrigins) {
+    if (allowedOrigin == '*') {
+      is_allowed = true;
+      break;
+    } else if (allowedOrigin.startsWith('/') && allowedOrigin.endsWith('/')) {
+      // Regex
+      if (RegExp(allowedOrigin.substring(1, allowedOrigin.length - 1)).test(origin)) {
+        is_allowed = true;
+        break;
+      }
+    } else {
+      if (origin === allowedOrigin) {
+        if (allowedOrigins.includes(origin)) {
+          is_allowed = true;
+          break;
+        }
+      }
+    }
+  }
+  if (is_allowed) {
     corsHeaders['Access-Control-Allow-Origin'] = origin;
   }
 
@@ -114,6 +312,91 @@ const setCorsHeaderToResponse = async (
     statusText: response.statusText,
     headers: new_headers,
   });
+};
+
+const sendCommentReplyNotificationEmail = async (
+  mailer: SendEmail,
+  original_comment: string,
+  original_comment_markdown: string,
+  original_timestamp: number,
+  original_email: string,
+  original_author_name: string,
+  replied_comment: string,
+  replied_comment_markdown: string,
+  replied_timestamp: number,
+  replied_author_name: string,
+  locale: string = 'en-US'
+): Promise<boolean> => {
+  const fillInfo = (text: string, is_markdown: boolean) =>
+    text
+      .replaceAll('$ORIGINAL_COMMENT_TIME', new Date(original_timestamp).toLocaleString(locale))
+      .replaceAll('$ORIGINAL_COMMENT', is_markdown ? original_comment_markdown : original_comment)
+      .replaceAll('$REPLIED_COMMENT_TIME', new Date(replied_timestamp).toLocaleString(locale))
+      .replaceAll('$REPLIED_COMMENT', is_markdown ? replied_comment_markdown : replied_comment)
+      .replaceAll('$REPLIED_AUTHOR_NAME', replied_author_name);
+
+  const html_text = fillInfo(htmlCommentReply, false);
+  const plain_text = fillInfo(textCommentReply, true);
+
+  const msg = createMimeMessage();
+  msg.setSender({ name: 'SQYBI.com Comment System', addr: 'comment@sqybi.com' });
+  msg.setRecipient({ addr: original_email, name: original_author_name });
+  msg.setSubject('[SQYBI.com] Your Comment Received a Reply!');
+  msg.addMessage({
+    contentType: 'text/plain',
+    data: plain_text,
+  });
+  msg.addMessage({
+    contentType: 'text/html',
+    data: html_text,
+  });
+
+  var message = new EmailMessage('comment@sqybi.com', original_email, msg.asRaw());
+  try {
+    await mailer.send(message);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
+const sendCommentPostedNotificationEmail = async (
+  mailer: SendEmail,
+  original_comment: string,
+  original_comment_markdown: string,
+  original_timestamp: number,
+  original_email: string,
+  original_author_name: string,
+  locale: string = 'en-US'
+): Promise<boolean> => {
+  const fillInfo = (text: string, is_markdown: boolean) =>
+    text
+      .replaceAll('$ORIGINAL_COMMENT_TIME', new Date(original_timestamp).toLocaleString(locale))
+      .replaceAll('$ORIGINAL_COMMENT', is_markdown ? original_comment_markdown : original_comment);
+
+  const html_text = fillInfo(htmlCommon + htmlCommentPosted, false);
+  const plain_text = fillInfo(textCommentPosted, true);
+
+  const msg = createMimeMessage();
+  msg.setSender({ name: 'SQYBI.com Comment System', addr: 'comment@sqybi.com' });
+  msg.setRecipient({ addr: original_email, name: original_author_name });
+  msg.setSubject('[SQYBI.com] You Posted a New Comment!');
+  msg.addMessage({
+    contentType: 'text/plain',
+    data: plain_text,
+  });
+  msg.addMessage({
+    contentType: 'text/html',
+    data: html_text,
+  });
+
+  var message = new EmailMessage('comment@sqybi.com', original_email, msg.asRaw());
+  try {
+    await mailer.send(message);
+  } catch (e) {
+    return false;
+  }
+  return true;
 };
 
 const markdownToHtml = async (markdown: string): Promise<string> => {
@@ -197,14 +480,15 @@ export default {
 
         // Check parent comment existence
         let level = 0;
+        let parent_comment = null;
         if (event.parent_comment_id) {
           const query = env.COMMENT_DB.prepare(
-            'SELECT level \
+            'SELECT level, html_content, markdown_content, comment_timestamp_ms, author_email \
             FROM comments \
             WHERE id = ?1 \
             LIMIT 1'
           );
-          const parent_comment = await query.bind(event.parent_comment_id).first();
+          parent_comment = await query.bind(event.parent_comment_id).first();
           if (!parent_comment) {
             console.warn(`Parent comment ${event.parent_comment_id} not found`);
             return getReponse('Parent comment not found', { status: 404 });
@@ -222,6 +506,7 @@ export default {
           comment_timestamp_ms) \
           VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9)'
         );
+        const now = new Date().getTime();
         const result = await command
           .bind(
             event.article_id,
@@ -232,14 +517,59 @@ export default {
             event.website || '',
             event.content,
             html_content,
-            event.timestamp_ms || new Date().getTime()
+            event.timestamp_ms || now
           )
           .run();
         if (result.error) {
           throw new DatabaseError(result.error);
         }
-        console.log(result.results);
-        return getReponse('{}');
+
+        const response_data = {
+          success: true,
+          email_notification: {
+            author: 'skipped',
+            reply: 'skipped',
+          },
+        };
+        if (event.email) {
+          const mail_sent = await sendCommentPostedNotificationEmail(
+            env.MAILER,
+            html_content,
+            event.content,
+            event.timestamp_ms || now,
+            event.email,
+            event.author
+          );
+          if (mail_sent) {
+            response_data.email_notification.author = 'sent';
+            console.log(`Email sent to ${event.email}`);
+          } else {
+            response_data.email_notification.author = 'failed';
+            console.warn(`Failed to send email to ${event.email}`);
+          }
+        }
+        if (parent_comment && parent_comment.author_email) {
+          const mail_sent = await sendCommentReplyNotificationEmail(
+            env.MAILER,
+            parent_comment.html_content as string,
+            parent_comment.markdown_content as string,
+            parent_comment.comment_timestamp_ms as number,
+            parent_comment.author_email as string,
+            parent_comment.author_name as string,
+            html_content,
+            event.content,
+            event.timestamp_ms || now,
+            event.author
+          );
+          if (mail_sent) {
+            response_data.email_notification.reply = 'sent';
+            console.log(`Email sent to ${parent_comment.author_email}`);
+          } else {
+            response_data.email_notification.reply = 'failed';
+            console.warn(`Failed to send email to ${parent_comment.author_email}`);
+          }
+        }
+        return getReponse(JSON.stringify(response_data));
       }
 
       // GET request for fetching comments
